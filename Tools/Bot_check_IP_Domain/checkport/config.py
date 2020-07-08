51669f31e954b68f626bfd8ece31871f8bc87812
hungviet99 <44610020+hungviet99@users.noreply.github.com>
2020-07-08 15:43:32 +0700

    Update

---
+++ Tools/Bot_check_IP_Domain/checkport/config.py
@@ -0,0 +1,14 @@
+# Token bot telegraam 
+TOKEN = "801935210:AAGogdiDRj0a1eFqX-1_FHNflMr_hf5E-7A"
+
+# API shodan
+ApiKeyShodan = "72OE88Qc15ZUK9RaTsONsuEJPeCswGU3"
+
+
+# API rapidapi
+url = "https://niemdt.p.rapidapi.com/"
+
+headers = {
+    'x-rapidapi-host': "zozor54-whois-lookup-v1.p.rapidapi.com",
+    'x-rapidapi-key': "4a457f2c3dmsh10ab8f81210e5c1p1f9933jsn4ccad93e8e6f"
+}
