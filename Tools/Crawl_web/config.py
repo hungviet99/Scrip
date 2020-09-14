7779910b400d890588c028c2a844b3039db18475
hungviet99 <44610020+hungviet99@users.noreply.github.com>
2020-09-14 09:50:36 +0700

    update

--- Tools/Crawl_web/config.py
+++ Tools/Crawl_web/config.py
@@ -1,3 +1,6 @@
+#Nhập vào token bot
 TOKEN =
+#Nhập vào chat id của post
 CHAT_ID =
+#Nhập vào link web cần craw
 LINK_WEB =
--- Tools/Crawl_web/redis-tele.py
+++ Tools/Crawl_web/redis-tele.py
@@ -4,29 +4,34 @@ import config
 
 bot = telebot.TeleBot(config.TOKEN_TELE)
 
+# Kết nối tới db redis
 redis_db = redis.StrictRedis(host="localhost", port=6379, db=0)
 
 result = []
 cur,key=redis_db.scan(cursor=0, count=10)
 result.extend(key)
 
+#Xử lý dữ liệu chuyển từ byte sang string
 lists_key = []
 for i in result:
     cv_str = i.decode("utf-8")
     lists_key.append(cv_str)
 
+#Lấy dữ liệu từ redis về tác giả 
 list_auth = []
 for i in lists_key:
     a = redis_db.hget(i, "auth_post")
     auth = a.decode("utf-8")
     list_auth.append(auth)
 
+#Lấy dữ liệu từ redis về link post
 link_auth = []
 for i in lists_key:
     b = redis_db.hget(i, "link_auth")
     link = b.decode("utf-8")
     link_auth.append(link)
 
+#Xử lý dữ liệu để loại những author trùng lặp
 author = []
 for x in list_auth:
     if x not in author:
@@ -34,17 +39,27 @@ for x in list_auth:
 
 @bot.message_handler(commands=["author"])
 def send_devices(message):
-    for a in author:
-        bot.send_message(config.CHAT_ID, a ,parse_mode='Markdown')
+    """
+        Tạo lệnh author để lấy thông tin tác giả từ redis
+    """
+    for sauth in author:
+        bot.send_message(config.CHAT_ID, sauth ,parse_mode='Markdown')
 
 @bot.message_handler(commands=["post"])
 def send_devices(message):
+    """
+        Tạo lệnh post để lấy link 10 post mới nhất.
+    """
     for i in lists_key:
         a = redis_db.hget(i, "link_post")
         auth = a.decode("utf-8")
         bot.send_message(config.CHAT_ID, str(auth) ,parse_mode='Markdown')
+
 @bot.message_handler(commands=["link"])
 def send_devices(message):
+    """
+        Tạo lệnh link để lấy link của các author viết 10 bài gần nhất
+    """
     bot.send_message(config.CHAT_ID, str(link_auth) ,parse_mode='Markdown')
 
 bot.polling()
