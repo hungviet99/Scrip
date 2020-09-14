8ca9036a5b87ba79ae1325063b14e135c17b7ea9
hungviet99 <44610020+hungviet99@users.noreply.github.com>
2020-09-14 09:33:13 +0700

    Update crawl.py

--- Tools/Crawl_web/crawl.py
+++ Tools/Crawl_web/crawl.py
@@ -5,10 +5,17 @@ import redis
 import unidecode
 import config
 
+# Get html 
 re = requests.get(config.LINK_WEB)
 html_raw = re.text
 
-def crawl_link(html):
+def crawl_web(html):
+    """Lấy thông tin web
+    :param html: là đoạn html get được từ trang web cần lấy thông tin. 
+
+    :response: trả về danh sách các thông tin như: 
+               id post, post link, author name, author link. 
+    """
     lists=[]
     soup = BeautifulSoup('{}' .format(html), 'html.parser')
     artical = soup.find_all('article')
@@ -24,14 +31,23 @@ def crawl_link(html):
             lists.append(auth)
     return lists
 
-def info_web(list_id, token, chat_id):
+def write_data_redis(list_if, token, chat_id):
+    """Xử lý dữ liệu và ghi vào redis
+    :param list_if: là list thông tin về:
+                    id post, link post, author name, author link.
+
+    :param token,chat_id: Truyền vào token bot và chatid của telegram.
+    
+    :response: Xử lý dữ liệu ghi vào redis,
+                nếu có dữ liệu mới thì gửi lên telegram.
+    """
     lists_key = []
     redis_db = redis.StrictRedis(host="localhost", port=6379, db=0)
     list_byte_key = redis_db.keys()
     for i in list_byte_key:
         cv_str = i.decode("utf-8")
         lists_key.append(cv_str)
-    for i in list_id:
+    for i in list_if:
         post_id = i[0] 
         post_link = i[1] 
         author = i[2] 
@@ -48,6 +64,6 @@ def info_web(list_id, token, chat_id):
     return
 
 if __name__ == "__main__":
-    list_inf_id = crawl_link(html_raw)
+    list_inf_web = crawl_web(html_raw)
 
-    info_web(list_inf_id, config.TOKEN, config.CHAT_ID)
+    write_data_redis(list_inf_web, config.TOKEN, config.CHAT_ID)
