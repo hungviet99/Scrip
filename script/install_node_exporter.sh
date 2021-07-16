41c546e17264440fc7d49307d8391ca059a28266
hungviet99 <44610020+hungviet99@users.noreply.github.com>
2021-07-16 11:31:28 +0700

    Update install_node_exporter.sh

--- script/install_node_exporter.sh
+++ script/install_node_exporter.sh
@@ -20,7 +20,7 @@ f_check_os(){
 f_setup_exporter_centos(){
     if [ $(id -u) -eq 0 ]; then
         if f_check_os; then 
-            echo "\n############## ENABLE PORT 9100 AND DISABLE SELINUX ##############\n"
+            echo "############## ENABLE PORT 9100 AND DISABLE SELINUX ##############"
             sed -i 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/sysconfig/selinux
             sed -i 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/selinux/config
             setenforce 0 || echo "selinux is disable"
@@ -28,12 +28,12 @@ f_setup_exporter_centos(){
             firewall-cmd --reload
             yum install -y wget
         else
-            echo "\n############## ENABLE PORT 9100 ##############\n"
+            echo "############## ENABLE PORT 9100 ##############"
             apt install -y wget
             ufw allow 9100/tcp
             ufw reload
         fi
-        echo "############## DOWNLOAD SOURCE CODE NODE EXPORTER ##############\n"
+        echo "############## DOWNLOAD SOURCE CODE NODE EXPORTER ##############"
         useradd --no-create-home --shell /bin/false node_exporter
         cd /opt
         wget https://github.com/prometheus/node_exporter/releases/download/v0.18.1/node_exporter-0.18.1.linux-amd64.tar.gz
@@ -42,7 +42,7 @@ f_setup_exporter_centos(){
         chown node_exporter:node_exporter /usr/local/bin/node_exporter
         rm -rf node_exporter-0.18.1.linux-amd64*
 
-echo "############## CREATE SERVICE NODE EXPORTER ##############\n"
+echo "############## CREATE SERVICE NODE EXPORTER ##############"
 cat <<EOF >  /etc/systemd/system/node_exporter.service
 [Unit]
 Description=Node Exporter
@@ -68,9 +68,9 @@ EOF
 }
 f_main(){
     f_setup_exporter_centos
-    echo "\n       ################################\n"
-    echo "       ###### CAI DAT THANH CONG ######\n"
-    echo "       ################################\n"
+    echo "       ################################"
+    echo "       ###### CAI DAT THANH CONG ######"
+    echo "       ################################"
 }
 f_main
 exit
